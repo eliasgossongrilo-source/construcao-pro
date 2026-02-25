6bbca318785faa5475ecb936f364481e47a54b26
eliasgossongrilo-source <eliasgossongrilo@gmail.com>
2026-02-25 08:07:56 -0300

    b1

---
+++ packages/ui/dist/index.d.ts
@@ -0,0 +1,5 @@
+import { type ClassValue } from 'clsx';
+export declare function cn(...inputs: ClassValue[]): string;
+export { clsx } from 'clsx';
+export { cva, type VariantProps } from 'class-variance-authority';
+//# sourceMappingURL=index.d.ts.map
